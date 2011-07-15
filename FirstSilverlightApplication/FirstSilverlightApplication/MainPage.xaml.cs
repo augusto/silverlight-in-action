1fba3dd70118b0564d5bbd425d920542d81d43f4
unknown <arodriguez@.uk.ioko365.com>
2011-07-15 16:47:07 +0100

    added layout for the tweet list

--- FirstSilverlightApplication/FirstSilverlightApplication/MainPage.xaml.cs
+++ FirstSilverlightApplication/FirstSilverlightApplication/MainPage.xaml.cs
@@ -9,14 +9,47 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Shapes;
+using System.Xml.Linq;
+using System.Collections.ObjectModel;
 
 namespace FirstSilverlightApplication
 {
     public partial class MainPage : UserControl
     {
+        private ObservableCollection<Tweet> _tweets = new ObservableCollection<Tweet>();
+
         public MainPage()
         {
             InitializeComponent();
+            TweetList.ItemsSource = _tweets;
+        }
+        
+        private void GetTweets_Click(object sender, RoutedEventArgs e)
+        {
+            WebClient client = new WebClient();
+            client.DownloadStringCompleted += (s, ea) =>
+            {
+                System.Diagnostics.Debug.WriteLine(ea.Result);
+
+                XDocument doc = XDocument.Parse(ea.Result);
+                XNamespace ns = "http://www.w3.org/2005/Atom";
+                var items = from item in doc.Descendants(ns + "entry")
+                            select new Tweet()
+                            {
+                                Message = item.Element(ns + "title").Value,
+                                Image = new Uri((
+                                from XElement xe in item.Descendants(ns + "link")
+                                where xe.Attribute("type").Value == "image/png"
+                                select xe.Attribute("href").Value
+                                ).First<string>()),
+                            };
+                foreach (Tweet t in items)
+                {
+                    _tweets.Add(t);
+                }
+            };
+
+            client.DownloadStringAsync( new Uri("http://search.twitter.com/search.atom?q=silverlight")); 
         }
     }
 }
---
+++ FirstSilverlightApplication/FirstSilverlightApplication/Tweet.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace FirstSilverlightApplication
+{
+    public class Tweet
+    {
+        public string Message { get; set; }
+        public Uri Image { get; set; }
+
+        public override string ToString()
+        {
+            return Message;
+        }
+    }
+}
