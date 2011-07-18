17c3675d6c3763bc220d58ae1ab2989d13f2217b
Augusto Rodriguez <augusto.rodriguez@gmail.com>
2011-07-18 15:32:05 +0100

    HelloYou

---
+++ HelloYou/HelloYou/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace HelloYou
+{
+    public partial class MainPage : UserControl
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Submit_Click(object sender, RoutedEventArgs e)
+        {
+            var name = Input.Text;
+            SayHello.Text = "Hello " + name + "!!";
+            SayHello.Visibility = System.Windows.Visibility.Visible;
+        }
+
+        private void Input_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key.Equals(Key.Enter))
+            {
+                Submit_Click(this, new RoutedEventArgs());
+            }
+        }
+    }
+}
